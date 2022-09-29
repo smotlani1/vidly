361eb21a4df45d3151c140715dc3be9844582f89
Saqib Motlani <motlani.saqib@gmail.com>
2022-09-29 07:29:57 -0500

    Save Button added to movie form

--- src/components/movieForm.jsx
+++ src/components/movieForm.jsx
@@ -1,7 +1,18 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
-const MovieForm = ({ match }) => {
-  return <h1>Movie Form {match.params.id}</h1>;
+const MovieForm = ({ match, history }) => {
+  return (
+    <div>
+      <h1>Movie Form {match.params.id}</h1>
+      <button
+        className="btn btn-primary btn-sm"
+        onClick={() => history.push("/movies")}
+      >
+        Save
+      </button>
+    </div>
+  );
 };
 
 export default MovieForm;
