8d0e1194294e175acef83b1a2d13fc00a674b084
Saqib Motlani <motlani.saqib@gmail.com>
2022-09-28 19:41:26 -0500

    Added Routing

--- src/components/moviesTable.jsx
+++ src/components/moviesTable.jsx
@@ -1,10 +1,17 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 import Like from "./common/like";
 import Table from "./common/table";
 
 class MoviesTable extends Component {
   columns = [
-    { path: "title", label: "title" },
+    {
+      path: "title",
+      label: "title",
+      content: (movie) => (
+        <Link to={`/movies/${movie._id}`}>{movie.title}</Link>
+      ),
+    },
     { path: "genre.name", label: "Genre" },
     { path: "numberInStock", label: "Stock" },
     { path: "dailyRentalRate", label: "Rate" },
